fc1f4ed10f8ebde98f76a9e129fd42476b9d66e5
Michael Boyle <mbbexley@hotmail.co.uk>
2020-08-08 21:27:35 +0100

    Basic clock setting on startup

---
+++ Assets/Basics/Clock/Clock.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Clock : MonoBehaviour
+{
+    public Transform hoursTransform, minutesTransform, secondsTransform;
+    const float
+        degreesPerHour = 30f,
+        degreesPerMinute = 6f,
+        degreesPerSecond = 6f;
+
+
+    private void Awake()
+    {
+        hoursTransform.localRotation =
+            Quaternion.Euler(0f, DateTime.Now.Hour * degreesPerHour, 0f);
+        minutesTransform.localRotation =
+            Quaternion.Euler(0f, DateTime.Now.Minute * degreesPerMinute, 0f);
+        secondsTransform.localRotation =
+            Quaternion.Euler(0f, DateTime.Now.Second * degreesPerSecond, 0f);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
