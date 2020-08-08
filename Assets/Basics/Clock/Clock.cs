e1e1965599a1ba59166473ead89fc99e1ea725e6
Michael Boyle <mbbexley@hotmail.co.uk>
2020-08-08 21:33:10 +0100

    Wrap up our clock task

--- Assets/Basics/Clock/Clock.cs
+++ Assets/Basics/Clock/Clock.cs
@@ -6,25 +6,40 @@ using UnityEngine;
 public class Clock : MonoBehaviour
 {
     public Transform hoursTransform, minutesTransform, secondsTransform;
+    public bool continuous;
     const float
         degreesPerHour = 30f,
         degreesPerMinute = 6f,
         degreesPerSecond = 6f;
 
-
-    private void Awake()
+    void UpdateContinuous()
     {
+        TimeSpan time = DateTime.Now.TimeOfDay;
         hoursTransform.localRotation =
-            Quaternion.Euler(0f, DateTime.Now.Hour * degreesPerHour, 0f);
+            Quaternion.Euler(0f, (float)time.TotalHours * degreesPerHour, 0f);
         minutesTransform.localRotation =
-            Quaternion.Euler(0f, DateTime.Now.Minute * degreesPerMinute, 0f);
+            Quaternion.Euler(0f, (float)time.TotalMinutes * degreesPerMinute, 0f);
         secondsTransform.localRotation =
-            Quaternion.Euler(0f, DateTime.Now.Second * degreesPerSecond, 0f);
+            Quaternion.Euler(0f, (float)time.TotalSeconds * degreesPerSecond, 0f);
     }
 
-    // Update is called once per frame
-    void Update()
+    void UpdateDiscrete()
     {
-        
+        DateTime time = DateTime.Now;
+        hoursTransform.localRotation =
+            Quaternion.Euler(0f, time.Hour * degreesPerHour, 0f);
+        minutesTransform.localRotation =
+            Quaternion.Euler(0f, time.Minute * degreesPerMinute, 0f);
+        secondsTransform.localRotation =
+            Quaternion.Euler(0f, time.Second * degreesPerSecond, 0f);
+    }
+
+    private void Update()
+    {
+        if (continuous) {
+            UpdateContinuous();
+        } else {
+            UpdateDiscrete();
+        }
     }
 }
