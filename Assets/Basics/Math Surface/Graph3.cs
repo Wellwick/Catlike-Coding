8465aa01953db4fde586aa80076d7ee0b15ab977
Michael Boyle <mbbexley@hotmail.co.uk>
2020-08-09 15:19:49 +0100

    Make a ripple and other 3D graphs

--- Assets/Basics/Math Surface/Graph3.cs
+++ Assets/Basics/Math Surface/Graph3.cs
@@ -7,8 +7,17 @@ public class Graph3 : MonoBehaviour
     public Transform pointPrefab;
     [Range(10, 100)]
     public int resolution = 10;
+    public GraphFunctionName function;
 
     Transform[] points;
+    GraphFunction[] functions =
+    {
+        SineFunction,
+        Sine2DFunction, 
+        MultiSineFunction,
+        MultiSine2DFunction, 
+        Ripple
+    };
 
     private void Awake()
     {
@@ -17,36 +26,67 @@ public class Graph3 : MonoBehaviour
         Vector3 position;
         position.y = 0f;
         position.z = 0f;
-        points = new Transform[resolution];
-        for (int i = 0; i < points.Length; ++i) {
-            points[i] = Instantiate(pointPrefab);
-            position.x = (i + 0.5f) * step - 1f;
-            points[i].localPosition = position;
-            points[i].localScale = scale;
-            points[i].SetParent(transform, false);
+        points = new Transform[resolution * resolution];
+        for (int i = 0, z = 0; z < resolution; z++) {
+            position.z = (z + 0.5f) * step - 1f;
+            for (int x = 0; x < resolution; x++, i++) {
+                points[i] = Instantiate(pointPrefab);
+                position.x = (x + 0.5f) * step - 1f;
+                points[i].localPosition = position;
+                points[i].localScale = scale;
+                points[i].SetParent(transform, false);
+            }
         }
     }
 
     private void Update()
     {
         float t = Time.time;
+        GraphFunction f = functions[(int)function];
         for (int i = 0; i < points.Length; i++) {
             Vector3 position = points[i].localPosition;
-            position.y = MultiSineFunction(position.x, t);
+            position.y = f(position.x, position.z, t);
             points[i].localPosition = position;
         }
     }
 
-    private float SineFunction(float x, float t)
+    const float pi = Mathf.PI;
+
+    private static float SineFunction(float x, float z, float t)
     {
-        return Mathf.Sin(Mathf.PI * (x + t));
+        return Mathf.Sin(pi * (x + t));
     }
 
-    private float MultiSineFunction(float x, float t)
+    private static float MultiSineFunction(float x, float z, float t)
     {
-        float y = Mathf.Sin(Mathf.PI * (x + t));
-        y += Mathf.Sin(2f * Mathf.PI * (x + t)) / 2f;
+        float y = Mathf.Sin(pi * (x + t));
+        y += Mathf.Sin(2f * pi * (x + 2f * t)) * 0.5f;
         y *= 2f / 3f;
+        return y;
+    }
+
+    private static float Sine2DFunction(float x, float z, float t)
+    {
+        float y = Mathf.Sin(pi * (x + t));
+        y += Mathf.Sin(pi * (z + t));
+        y *= 0.5f;
+        return y;
+    }
+
+    private static float MultiSine2DFunction(float x, float z, float t)
+    {
+        float y = 4f * Mathf.Sin(pi * (x + z + t * 0.5f));
+        y += Mathf.Sin(pi * (x + t));
+        y += Mathf.Sin(2f * pi * (z + 2f * t)) * 0.5f;
+        y *= 1f / 5.5f;
+        return y;
+    }
+
+    private static float Ripple(float x, float z, float t)
+    {
+        float d = Mathf.Sqrt(x * x + z * z);
+        float y = Mathf.Sin(pi * (4f * d - t));
+        y /= 1f + 10f * d;
         return y;
     }
 }
