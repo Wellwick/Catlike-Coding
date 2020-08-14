7e192c9e7b42fd279169d7ce1ab49a7f1754a850
Michael Boyle <mbbexley@hotmail.co.uk>
2020-08-14 17:13:40 +0100

    Do the Frames tutorial

---
+++ Assets/Basics/Frames/FPSCounter.cs
@@ -0,0 +1,59 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FPSCounter : MonoBehaviour
+{
+    public int frameRange = 60;
+    public int AverageFPS { get; private set; }
+    public int HighestFPS { get; private set; }
+    public int LowestFPS { get; private set; }
+
+    int[] fpsBuffer;
+    int fpsBufferIndex;
+
+    private void InitializeBuffer()
+    {
+        if (frameRange <= 0) {
+            frameRange = 1;
+        }
+        fpsBuffer = new int[frameRange];
+        fpsBufferIndex = 0;
+    }
+
+    private void Update()
+    {
+        if (fpsBuffer == null || fpsBuffer.Length != frameRange) {
+            InitializeBuffer();
+        }
+        UpdateBuffer();
+        CalculateFPS();
+    }
+
+    private void UpdateBuffer()
+    {
+        fpsBuffer[fpsBufferIndex++] = (int)(1f / Time.unscaledDeltaTime);
+        if (fpsBufferIndex >= frameRange) {
+            fpsBufferIndex = 0;
+        }
+    }
+
+    private void CalculateFPS()
+    {
+        int sum = 0;
+        int highest = 0;
+        int lowest = int.MaxValue;
+        for (int i = 0; i < frameRange; i++) {
+            sum += fpsBuffer[i];
+            if (fpsBuffer[i] > highest) {
+                highest = fpsBuffer[i];
+            }
+            if (fpsBuffer[i] < lowest) {
+                lowest = fpsBuffer[i];
+            }
+        }
+        AverageFPS = sum / frameRange;
+        HighestFPS = highest;
+        LowestFPS = lowest;
+    }
+}
