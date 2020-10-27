ae0a3d6b15e6e63e19812985b13cb73a35f2f834
Michael Boyle <mbbexley@hotmail.co.uk>
2020-10-27 20:49:41 +0000

    Make the scene have some obstacles

--- Assets/Movement/OrbitCamera/CameraMoveSphere.cs
+++ Assets/Movement/OrbitCamera/CameraMoveSphere.cs
@@ -65,7 +65,17 @@ public class CameraMoveSphere : MonoBehaviour {
         playerInput.x = Input.GetAxis("Horizontal");
         playerInput.y = Input.GetAxis("Vertical");
         playerInput = Vector2.ClampMagnitude(playerInput, 1f);
-        desiredVelocity = new Vector3(playerInput.x, 0f, playerInput.y) * maxSpeed;
+        if (playerInputSpace) {
+            Vector3 forward = playerInputSpace.forward;
+            forward.y = 0f;
+            forward.Normalize();
+            Vector3 right = playerInputSpace.right;
+            right.y = 0f;
+            right.Normalize();
+            desiredVelocity = (forward * playerInput.y + right * playerInput.x) * maxSpeed;
+        } else {
+            desiredVelocity = new Vector3(playerInput.x, 0f, playerInput.y) * maxSpeed;
+        }
 
         desiredJump |= Input.GetButtonDown("Jump");
 
